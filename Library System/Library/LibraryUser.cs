cd1f61f1ab0e4a40ca97b9f5eea1f7688ced8fa2
Sara Elghazaly <UG_31094092@F-ENG.TANTA.EDU.EG>
2025-02-08 22:56:27 +0200

    Add files via upload

---
+++ Library System/Library/LibraryUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class LibraryUser : User
+    {
+        public LibraryUser(string name)
+        {
+            Name = name;
+        }
+        public LibraryCard Card { get; set; }
+
+
+        public void BorrowBooks(Book book, Library library)
+        {
+            library.BorrowBook(book);
+        }
+    }
+}
